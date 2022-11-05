e48d204744f16bfb3c5b9dfe696e9cc362cc1fe3
alan295757 <113963477+alan295757@users.noreply.github.com>
2022-11-05 14:00:24 +0800

    建立Firstscipt

---
+++ unity20220929/Assets/Scripts/FirstScripts.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace JAY
+{
+    /// <summary>
+    /// 第一個程式
+    /// </summary>
+    public class FirstScripts : MonoBehaviour
+    {
+        #region 註解區域
+        // 縮排 快捷鍵 Tab
+        // 格式化文件 Ctrl + K D
+        // 單行註解
+        #endregion
+
+        #region 事件區域
+        
+
+        #endregion
+    }
+}
+
+ 
