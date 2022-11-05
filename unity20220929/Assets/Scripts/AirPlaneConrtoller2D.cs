7f24a133b70365711dfbba288a037191226ea261
alan295757 <113963477+alan295757@users.noreply.github.com>
2022-11-05 18:12:48 +0800

    完成飛機控制器

---
+++ unity20220929/Assets/Scripts/AirPlaneConrtoller2D.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace JAY
+{
+    /// <summary>
+    /// 飛機控制器 2D 模式
+    /// </summary>
+    public class AirPlaneConrtoller2D : MonoBehaviour
+    {
+        [Header("移動速度")]
+        [SerializeField, Range(0, 10)]
+        private float speedVertical = 3.5f;
+        [SerializeField, Range(0, 10)]
+        private float speedHorizontal = 4f;
+        [Header("圖片")]
+        [SerializeField]
+        private Sprite pictureUp;
+        [SerializeField]
+        private Sprite pictureMiddle;
+        [SerializeField]
+        private Sprite pictureDown;
+    }
+}
--- unity20220929/Assets/Scripts/FirstScripts.cs
+++ unity20220929/Assets/Scripts/FirstScripts.cs
@@ -12,9 +12,25 @@ namespace JAY
         // 格式化文件 Ctrl + K D
         // 單行註解
         #endregion
+            // 喚醒事件
+        private void Awake()
+        {
+            print("哈囉");
+        }
 
+        // 開始事件
+        private void Start()
+        {
+            print("<color=yellow>這是開始事件</color>");
+        }
+
+        // 更新事件：在 Start 後執行 執行次數約 60 FPS
+        private void Update()
+        {
+            print("<color=red>更新事件！</color>");
+        }
         #region 事件區域
-        
+
 
         #endregion
     }
