9401269d864b625e6a328f7aeafc9987bf87c252
alan295757 <113963477+alan295757@users.noreply.github.com>
2022-11-06 12:22:27 +0800

    完成空白鍵生成子彈

--- unity20220929/Assets/Scripts/FireSystemBase.cs
+++ unity20220929/Assets/Scripts/FireSystemBase.cs
@@ -13,8 +13,10 @@ namespace JAY
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
 
-        //生成子彈
-        private void SpawnBullet()
+        // 自訂方法
+        // 生成子彈
+        // protected 保護級別：允許子類別存取
+        protected void SpawnBullet()
         {
             // 實例化(物件 座標 角度)
             // 生成 子彈預製物 角度與座標跟子彈生成點相同
--- unity20220929/Assets/Scripts/PlayerFireSystem.cs
+++ unity20220929/Assets/Scripts/PlayerFireSystem.cs
@@ -7,6 +7,14 @@ namespace JAY
     /// </summary>
     public class PlayerFireSystem : FireSystemBase
     {
-
+        // 偵測玩家的輸入行為：鍵盤 滑鼠 觸控 搖桿
+        private void Update()
+        {
+            // 如果 玩家 按下空白鍵 就生成子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
     }
 }
